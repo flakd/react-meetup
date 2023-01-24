504b301b1408ba9948b9bf29967124e59518b6d2
Flak DiNenno <flak@dinenno.com>
2023-01-24 11:49:12 -0500

    implemented submit form to Firebase

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,11 +1,33 @@
-import classes from './NewMeetupForm.module.css'
+import {useRef} from 'react';
+import classes from './NewMeetupForm.module.css';
 
-import Card from '../ui/Card'
+import Card from '../ui/Card';
+import userEvent from '@testing-library/user-event';
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
+	const titleInputRef = useRef();
+	const imageInputRef = useRef();
+	const addressInputRef = useRef();
+	const descriptionInputRef = useRef();
+
 	function handleSubmit(e) {
-		e.preventDefault()
-		console.log('I clicked the SUBMIT button')
+		e.preventDefault();
+		console.log('I clicked the SUBMIT button');
+
+		const enteredTitle = titleInputRef.current.value;
+		const enteredImage = imageInputRef.current.value;
+		const enteredAddress = addressInputRef.current.value;
+		const enteredDescription = descriptionInputRef.current.value;
+
+		const meetupData = {
+			title: enteredTitle,
+			image: enteredImage,
+			address: enteredAddress,
+			description: enteredDescription,
+		};
+
+		console.log(meetupData);
+		props.onAddMeetup(meetupData);
 	}
 
 	return (
@@ -20,24 +42,29 @@ function NewMeetupForm() {
 						type='text'
 						required
 						id='title'
+						ref={titleInputRef}
 					/>
 					<label htmlFor='image'>Image URL</label>
 					<input
 						type='url'
 						required
 						id='image'
+						ref={imageInputRef}
+						value='	http://google.com'
 					/>
 					<label htmlFor='address'>Address</label>
 					<input
 						type='text'
 						required
 						id='address'
-					/>{' '}
+						ref={addressInputRef}
+					/>
 					<label htmlFor='description'>Description</label>
 					<textarea
 						required
 						rows='5'
 						id='description'
+						ref={descriptionInputRef}
 					/>
 				</div>
 				<div className={classes.actions}>
@@ -45,7 +72,7 @@ function NewMeetupForm() {
 				</div>
 			</form>
 		</Card>
-	)
+	);
 }
 
-export default NewMeetupForm
+export default NewMeetupForm;
--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,11 +1,24 @@
-import NewMeetupForm from '../components/meetups/NewMeetupForm'
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetingsPage() {
+	function handleAddMeetup(meetupData) {
+		fetch(
+			'https://react-meetup-fd19a-default-rtdb.firebaseio.com/meetups.json',
+			{
+				method: 'POST',
+				body: JSON.stringify(meetupData),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+		);
+	}
+
 	return (
 		<section>
 			<h1>Add New Meetup</h1>
-			<NewMeetupForm />
+			<NewMeetupForm onAddMeetup={handleAddMeetup} />
 		</section>
-	)
+	);
 }
-export default NewMeetingsPage
+export default NewMeetingsPage;
