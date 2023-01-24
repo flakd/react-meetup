a76bda04ca7d5db590bdfafe2d0967082f2cdc5c
Flak DiNenno <flak@dinenno.com>
2023-01-23 20:58:10 -0500

    printed titles/dummy meetups on 'All Meetups'

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,19 +1,21 @@
 import {Link} from 'react-router-dom'
 
+import classes from './MainNavigation.module.css'
+
 function MainNavigation() {
 	return (
-		<header>
-			<div>React Meetups</div>
+		<header className={classes.header}>
+			<div className={classes.logo}>React Meetups</div>
 			<nav>
 				<ul>
 					<li>
 						<Link to='/'>All Meetups</Link>
 					</li>
 					<li>
-						<Link to='/favorites'>Favorite Meetups</Link>
+						<Link to='/favorites'>My Favorites</Link>
 					</li>
 					<li>
-						<Link to='/new-meetups'>All Meetups</Link>
+						<Link to='/new-meetups'>Add New Meetup</Link>
 					</li>
 				</ul>
 			</nav>
